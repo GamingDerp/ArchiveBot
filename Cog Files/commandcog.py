cf9a522d1f3415df7d57665a0905bc53e1a3f608
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2023-09-11 07:21:10 -0500

    Updated commandcog.py

--- Cog Files/commandcog.py
+++ Cog Files/commandcog.py
@@ -202,15 +202,15 @@ class CommandCog(commands.Cog):
         if user1.id == derp_id and user2.id == oni_id or user1.id == oni_id and user2.id == derp_id:
             e.description = f"**{user1.mention}** and **{user2.mention}** are a **100%** match! :flushed:"
             await interaction.response.send_message(embed=e)
-        elif user1.id == derp_id and user2.id == terra_id or user1.id == terra_id and user2.id == derp_id:
-            e.description = f"**{user1.mention}** and **{user2.mention}** are a **99.99%** match! :flushed:"
-            await interaction.response.send_message(embed=e)
+        elif user1.id == derp_id and user2.id != oni_id or user1.id == oni_id and user2.id != derp_id:
+            e.description = f"**{user1.mention}** and **{user2.mention}** are a **0%** match! :flushed:"
+            await ctx.send(embed=e)
         elif user1.id == jimmy_id and user2.id == zuko_id or user1.id == zuko_id and user2.id == jimmy_id:
             e.description = f"**{user1.mention}** and **{user2.mention}** are a **100%** match! :flushed:"
             await interaction.response.send_message(embed=e)
         else:
             e.description = f"**{user1.mention}** and **{user2.mention}** are a **{love_rate}%** match! :flushed:"
-            await interaction.response.send_message(embed=e)   
+            await interaction.response.send_message(embed=e)
                                  
     # WhoIs Command
     @bot.tree.command(description="Sends information about a users account")
